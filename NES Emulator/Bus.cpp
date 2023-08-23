7864b03ba9e3a6ab2ea95d6e586d4d4b12f6159b
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-08-23 18:53:21 -0400

    Added addressing modes

--- NES Emulator/Bus.cpp
+++ NES Emulator/Bus.cpp
@@ -1,1 +1,28 @@
 #include "Bus.h"
+
+Bus::Bus()
+{
+	// Clear RAM
+	for (auto& i : ram) { i = 0; }
+
+	cpu.ConnectBus(this);
+}
+
+Bus::~Bus()
+= default;
+
+uint8_t Bus::read(uint16_t addr, bool bReadOnly)
+{
+	if (addr >= 0x0000 && addr <= 0xFFFF) {
+		return ram[addr];
+	}
+
+	return 0;
+}
+
+void Bus::write(uint16_t addr, uint8_t data)
+{
+	if (addr >= 0x0000 && addr <= 0xFFFF) {
+		ram[addr] = data;
+	}
+}
--- NES Emulator/Bus.h
+++ NES Emulator/Bus.h
@@ -1,15 +1,23 @@
 #pragma once
+#include <array>
 #include <cstdint>
 
 #include "CPU.h"
 
 class Bus
 {
-
 public:
+	Bus();
+	~Bus();
+
+public: // Bus devices
 	CPU cpu;
 
-public:
+	std::array<uint8_t, 65536> ram;
+
+
+
+public: // Bus read and write
 	void write(uint16_t addr, uint8_t data);
 	uint8_t read(uint16_t addr, bool bReadOnly = false);
 };
